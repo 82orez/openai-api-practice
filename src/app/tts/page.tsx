5ff62d110d0423ba5208c993cc896729faf0ab1d
82orez <82orez@naver.com>
2025-01-22 11:00:26 +0900

    tts answering...1

--- src/app/tts/page.tsx
+++ src/app/tts/page.tsx
@@ -2,26 +2,38 @@
 
 import axios from "axios";
 import Link from "next/link";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { useState } from "react";
 
 export default function TTSPage() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div className={"flex flex-col justify-center gap-5"}>
       <div>TTSPage - 텍스트를 오디오 파일로</div>
 
       <div>
-        <button
-          onClick={async () => {
-            const res = await axios("/api/tts", {});
-            const result = await res.data;
-            if (result.success) {
-              window.location.href = result.fileUrl;
-            } else {
-              alert("Failed to generate audio.");
-            }
-            return;
-          }}>
-          Start making Mp3 file!
-        </button>
+        {isLoading ? (
+          <AiOutlineLoading3Quarters className={"animate-spin text-xl"} />
+        ) : (
+          <button
+            onClick={async () => {
+              setIsLoading(true);
+
+              const res = await axios("/api/tts", {});
+              const result = await res.data;
+              if (result.success) {
+                window.location.href = result.fileUrl;
+              } else {
+                alert("Failed to generate audio.");
+              }
+
+              setIsLoading(false);
+              return;
+            }}>
+            Start making Mp3 file!
+          </button>
+        )}
       </div>
 
       <Link href={"/"}>To Home!</Link>
