aff1de82c09483559e40a3965f8280c9cfbafa94
82orez <82orez@naver.com>
2025-01-28 12:35:52 +0900

    record-text-1

--- src/app/record-text/page.tsx
+++ src/app/record-text/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 // * 크게 2가지 api 가 필요
-// 녹음한 내용을 서버에 저장하는 api
-// 서버에 저장된 음성 파일에서 text 를 추출하는 api - whisper
+// 녹음한 내용을 서버에 저장하는 api: /api/recordings
+// 서버에 저장된 음성 파일에서 text 를 추출하는 api: /api/transcribe - whisper
 
 import { useRecordingStore } from "@/stores/recordingStore";
 import { useState } from "react";
@@ -11,7 +11,6 @@ import Link from "next/link";
 const AudioRecorder = () => {
   const { isRecording, startRecording, stopRecording } = useRecordingStore();
   const [audioURL, setAudioURL] = useState<string | null>(null);
-  const [uploadedURL, setUploadedURL] = useState<string | null>(null);
   const [transcription, setTranscription] = useState<string | null>(null);
   const [isProcessing, setIsProcessing] = useState(false);
 
@@ -39,8 +38,6 @@ const AudioRecorder = () => {
         // 서버에 업로드 된 후, 해당 녹음 파일의 위치 정보가 담긴 공개 url 을 반환 받아 옮.
         const result = await uploadResponse.json();
         if (result.url) {
-          setUploadedURL(result.url);
-
           // Supabase 에 업로드된 파일에서 Whisper API 로 보내서 텍스트 추출을 요청.
           const transcriptResponse = await fetch("/api/transcribe", {
             method: "POST",
