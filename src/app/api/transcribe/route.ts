3ff32d1fd8cfde0f77aed3674b3496415c8fca5a
82orez <82orez@naver.com>
2025-01-28 12:06:21 +0900

    record-text

--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -6,31 +6,33 @@ import path from "path";
 
 export async function POST(req: NextRequest) {
   try {
+    // 오디오 파일의 위치 정보를 받아 옮.
     const { fileUrl } = await req.json();
     if (!fileUrl) {
       return NextResponse.json({ error: "No file URL provided" }, { status: 400 });
     }
 
-    // 파일을 다운로드하여 로컬에서 처리
+    // 해당 녹음 파일을 다운로드하여 로컬에서 처리
     const response = await axios.get(fileUrl, { responseType: "arraybuffer" });
     const audioBuffer = Buffer.from(response.data);
 
-    // 임시 파일 저장
+    // 임시 파일 폴더에 저장
     const tempFilePath = path.join("/tmp", `recording-${Date.now()}.mp3`);
     fs.writeFileSync(tempFilePath, audioBuffer);
 
     const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! });
 
-    // Whisper API 에 파일 업로드
+    // Whisper API 에 파일을 보내고 text 추출을 요청함.
     const transcriptionResponse = await openai.audio.transcriptions.create({
       file: fs.createReadStream(tempFilePath), // 파일 스트림 전송
       model: "whisper-1",
       // language: "en",
     });
 
-    // 임시 파일 삭제
+    // 임시로 저장했던 파일 삭제
     fs.unlinkSync(tempFilePath);
 
+    // whisper api 가 추출한 text 를 클라이언트로 반환함.
     return NextResponse.json({ text: transcriptionResponse.text });
   } catch (error) {
     console.error("Transcription Error:", error);
--- src/app/record-text/page.tsx
+++ src/app/record-text/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+// * 크게 2가지 api 가 필요
+// 녹음한 내용을 서버에 저장하는 api
+// 서버에 저장된 음성 파일에서 text 를 추출하는 api - whisper
+
 import { useRecordingStore } from "@/stores/recordingStore";
 import { useState } from "react";
 import Link from "next/link";
@@ -26,24 +30,27 @@ const AudioRecorder = () => {
         const formData = new FormData();
         formData.append("audio", new File([audioBlob], "recording.mp3"));
 
-        // 서버에 오디오 업로드 및 텍스트 변환 요청
+        // 서버에 오디오 업로드 요청
         const uploadResponse = await fetch("/api/recordings", {
           method: "POST",
           body: formData,
         });
 
+        // 서버에 업로드 된 후, 해당 녹음 파일의 위치 정보가 담긴 공개 url 을 반환 받아 옮.
         const result = await uploadResponse.json();
         if (result.url) {
           setUploadedURL(result.url);
 
-          // Supabase 에 업로드된 파일에서 Whisper API 로 텍스트 추출
+          // Supabase 에 업로드된 파일에서 Whisper API 로 보내서 텍스트 추출을 요청.
           const transcriptResponse = await fetch("/api/transcribe", {
             method: "POST",
             body: JSON.stringify({ fileUrl: result.url }),
             headers: { "Content-Type": "application/json" },
           });
 
+          // 받아온 text 를 처리.
           const transcriptResult = await transcriptResponse.json();
+
           if (transcriptResult.text) {
             setTranscription(transcriptResult.text);
           } else {
