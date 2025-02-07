1a9364eba00d8225e1e65bf7a437dc1981300f5a
82orez <82orez@naver.com>
2025-02-08 07:50:27 +0900

    authorization

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,6 +16,13 @@ export const authOptions: NextAuthOptions = {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      authorization: {
+        params: {
+          prompt: "select_account",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
     Naver({
       clientId: process.env.NAVER_CLIENT_ID || "",
