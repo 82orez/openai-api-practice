98495f267eb3467010cb6110175d939e018729fc
82orez <82orez@naver.com>
2025-02-18 03:08:18 +0900

    schema upgraded for Recording

--- src/types_db.ts
+++ src/types_db.ts
@@ -42,27 +42,171 @@ export type Database = {
         }
         Relationships: []
       }
-      test: {
+      Account: {
         Row: {
-          age: number | null
-          created_at: string
-          id: number
-          name: string
-          updated_at: string | null
+          access_token: string | null
+          createdAt: string
+          expires_at: number | null
+          id_token: string | null
+          provider: string
+          providerAccountId: string
+          refresh_token: string | null
+          refresh_token_expires_in: number | null
+          scope: string | null
+          session_state: string | null
+          token_type: string | null
+          type: string
+          updatedAt: string
+          userId: string
         }
         Insert: {
-          age?: number | null
-          created_at?: string
-          id?: number
-          name: string
-          updated_at?: string | null
+          access_token?: string | null
+          createdAt?: string
+          expires_at?: number | null
+          id_token?: string | null
+          provider: string
+          providerAccountId: string
+          refresh_token?: string | null
+          refresh_token_expires_in?: number | null
+          scope?: string | null
+          session_state?: string | null
+          token_type?: string | null
+          type: string
+          updatedAt: string
+          userId: string
         }
         Update: {
-          age?: number | null
-          created_at?: string
-          id?: number
-          name?: string
-          updated_at?: string | null
+          access_token?: string | null
+          createdAt?: string
+          expires_at?: number | null
+          id_token?: string | null
+          provider?: string
+          providerAccountId?: string
+          refresh_token?: string | null
+          refresh_token_expires_in?: number | null
+          scope?: string | null
+          session_state?: string | null
+          token_type?: string | null
+          type?: string
+          updatedAt?: string
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Account_userId_fkey"
+            columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "User"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      Recording: {
+        Row: {
+          createdAt: string
+          fileUrl: string
+          id: string
+          userId: string
+        }
+        Insert: {
+          createdAt?: string
+          fileUrl: string
+          id: string
+          userId: string
+        }
+        Update: {
+          createdAt?: string
+          fileUrl?: string
+          id?: string
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Recording_userId_fkey"
+            columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "User"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      Session: {
+        Row: {
+          createdAt: string
+          expires: string
+          sessionToken: string
+          updatedAt: string
+          userId: string
+        }
+        Insert: {
+          createdAt?: string
+          expires: string
+          sessionToken: string
+          updatedAt: string
+          userId: string
+        }
+        Update: {
+          createdAt?: string
+          expires?: string
+          sessionToken?: string
+          updatedAt?: string
+          userId?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Session_userId_fkey"
+            columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "User"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      User: {
+        Row: {
+          createdAt: string
+          email: string
+          emailVerified: string | null
+          id: string
+          image: string | null
+          name: string | null
+          updatedAt: string
+        }
+        Insert: {
+          createdAt?: string
+          email: string
+          emailVerified?: string | null
+          id: string
+          image?: string | null
+          name?: string | null
+          updatedAt: string
+        }
+        Update: {
+          createdAt?: string
+          email?: string
+          emailVerified?: string | null
+          id?: string
+          image?: string | null
+          name?: string | null
+          updatedAt?: string
+        }
+        Relationships: []
+      }
+      VerificationToken: {
+        Row: {
+          expires: string
+          identifier: string
+          token: string
+        }
+        Insert: {
+          expires: string
+          identifier: string
+          token: string
+        }
+        Update: {
+          expires?: string
+          identifier?: string
+          token?: string
         }
         Relationships: []
       }
