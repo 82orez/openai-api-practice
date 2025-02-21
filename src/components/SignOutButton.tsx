2cfbd948d6abb7bf918c0e53ed259a541d3c7903
82orez <82orez@naver.com>
2025-02-21 17:18:28 +0900

    sign out

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { PiSignOut } from "react-icons/pi";
+import { useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function SignOutButton() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <button
-      className={"max-w-[120px] rounded-xl border-2 bg-red-300 p-2"}
-      onClick={() => {
-        // * 로그아웃 이후 redirect 할 경로설정.
-        signOut({ callbackUrl: "/" });
-      }}>
-      SignOut
-    </button>
+    <div className={"flex max-w-[120px] items-center justify-center gap-1 rounded-xl border-2 p-2"}>
+      {isLoading ? <AiOutlineLoading3Quarters className={"animate-spin"} /> : <PiSignOut size={25} />}
+
+      <button
+        onClick={() => {
+          setIsLoading(true);
+
+          // * 로그아웃 이후 redirect 할 경로설정.
+          signOut({ callbackUrl: "/" });
+        }}>
+        Sign Out
+      </button>
+    </div>
   );
 }
